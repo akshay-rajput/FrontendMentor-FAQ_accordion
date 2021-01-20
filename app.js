4e66e24fc1b0e2e54d5f1d1f9fa2f1324216ba0d
Akshay <akshayrajput102@gmail.com>
2021-01-20 16:33:08 +0530

    find siblings of selected question and remove active

--- app.js
+++ app.js
@@ -1,15 +1,38 @@
 var question_links = document.getElementsByClassName("btn-expand");
+var answers = document.getElementsByClassName('answer');
 var i;
 
 for (i = 0; i < question_links.length; i++) {
   question_links[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.add("active");
 
-    var answer = this.nextElementSibling;
-    if (answer.style.maxHeight) {
-      answer.style.maxHeight = null;
-    } else {
-      answer.style.maxHeight = answer.scrollHeight + "px";
-    }
+    let getSiblings = function (element) {
+        // for collecting siblings
+        let otherQuestions = []; 
+
+        // if no parent, return no sibling
+        if(!element.parentNode) {
+            return otherQuestions;
+        }
+        
+        // first child of the parent node
+        let sibling  = element.parentNode.firstChild;
+        
+        // collecting siblings
+        while (sibling) {
+            if (sibling.nodeType === 1 && sibling !== element) {
+                otherQuestions.push(sibling);
+            }
+            sibling = sibling.nextSibling;
+        }
+        return otherQuestions;
+    };
+
+    // remove active class from other siblings
+    let allQuestions =  getSiblings(this.parentNode);
+    allQuestions.forEach(question => {
+        let questionDiv = question.querySelector(`.btn-expand`);
+        questionDiv.classList.remove('active');
+    });
   });
 }
